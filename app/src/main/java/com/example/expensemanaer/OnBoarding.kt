d8e6979526dddb83e7323777711c7f0fd7f54777
Rahul yadav <dk283532@gmail.com>
2021-05-10 17:38:49 +0530

    Navcontroller issue resolve

--- app/src/main/java/com/example/expensemanaer/OnBoarding.kt
+++ app/src/main/java/com/example/expensemanaer/OnBoarding.kt
@@ -1,5 +1,4 @@
 package com.example.expensemanaer
-
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
@@ -9,10 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_on_boarding.*
-
-
 class OnBoarding : Fragment() {
-
     lateinit var sharedPreferences: SharedPreferences
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -21,24 +17,17 @@ class OnBoarding : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_on_boarding, container, false)
     }
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        sharedPreferences =
-                this.requireActivity().getSharedPreferences("login", Context.MODE_PRIVATE)
+        sharedPreferences =this.requireActivity().getSharedPreferences("login", Context.MODE_PRIVATE)
         continu.setOnClickListener {
             findNavController().navigate(OnBoardingDirections.actionOnBoardingToUpcomingFragment())
             val name = Name.editText?.text.toString()
             val budget = user_month_budget.editText?.text.toString()
             val income = monthlyincome.editText?.text.toString()
             saveCredentials(name, budget, income)
-
-
-            
         }
-
     }
-
     private fun saveCredentials(name: String, budget: String, income: String) {
         sharedPreferences.edit().putString("Name", name).apply()
         sharedPreferences.edit().putString("Budget", budget).apply()
